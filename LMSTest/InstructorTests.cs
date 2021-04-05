694117c6a9b2264dbfc4c2e9a9d851cb08c33597
Cameron Martin <camgmartin@gmail.com>
2021-04-05 15:30:05 -0600

    Two unit tests working.

--- LMSTest/InstructorTests.cs
+++ LMSTest/InstructorTests.cs
@@ -4,17 +4,107 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LMS.Data;
 using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace LMSTest
 {
     [TestClass]
     public class InstructorTests
     {
-        [TestClass]
-        public class UnitTest1
+        [TestMethod]
+        public void StudentRegisterCourse()
         {
-           
+            /** Arrange **/
+            var optionsBuilder = new DbContextOptionsBuilder<LMSContext>().UseSqlServer("Data Source=titan.cs.weber.edu,10433;Initial Catalog=LMS_BLUE;User ID=LMS_BLUE;Password=BTlms2021!");
+            LMS.Data.LMSContext context = new LMSContext(optionsBuilder.Options);
+            int userID = 1010;
+
+            var pageModel = new LMS.Pages.Registrations.IndexModel(context)
+            {
+                UserID = userID,
+                Registration = new Registration(),
+            };
+
+            /** Act **/
+            List<Registration> registrationRecords;
+            registrationRecords = context.Registration.Where(r => r.Student == userID).ToList();
+            int preCount = registrationRecords.Count();
+
+            int courseID = 10;
+            pageModel.AddCourse(courseID);
+
+            registrationRecords = context.Registration.Local.Where(r => r.Student == userID).ToList();
+            int postCount = registrationRecords.Count();
+
+            /** Compare **/
+            Assert.AreNotEqual(preCount, postCount);
+        }
+
+        [TestMethod]
+        public async Task CourseCreateTest()
+        {
+            // Arrange
+            var optionsBuilder = new DbContextOptionsBuilder<LMSContext>().UseSqlServer("Data Source=titan.cs.weber.edu,10433;Initial Catalog=LMS_BLUE;User ID=LMS_BLUE;Password=BTlms2021!");
+            var _context = new LMSContext(optionsBuilder.Options);
+
+            LMS.Pages.Courses.CreateModel model = new LMS.Pages.Courses.CreateModel(_context);
+
+            User user = new User()
+            {
+                FirstName = "David",
+                LastName = "Smith",
+                ID = 4,
+                IsInstructor = true,
+            };
+
+            Course insertCourse = new Course()
+            {
+                Number = 3030,
+                Name = "InstructorTest",
+                Department = "CS",
+                Description = "Instructor test course",
+                Location = "Test Location",
+                CreditHours = 4,
+                Capacity = 0
+            };
+
+            model.Course = insertCourse;
+            model.Monday = true;
+            model.Wednesday = true;
+            model.Friday = true;
+            model.StartTime = new DateTime(2021, 3, 22, 6, 30, 0);
+            model.EndTime = new DateTime(2021, 3, 22, 8, 50, 0);
+
+            // Act
+            int precount = _context.Course.Where(c => c.InstructorID == user.ID).ToList().Count();
+            await model.CreateCourse(user);
+            int postcount = _context.Course.Where(c => c.InstructorID == user.ID).ToList().Count();
+
+            // Assert
+            Assert.AreNotEqual(precount, postcount);
+
+            // Cleanup
+            _context.Course.Remove(insertCourse);
+            _context.SaveChanges();
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDistributedMemoryCache();
+            services.AddHttpContextAccessor();
+            services.AddSession(options =>
+            {
+                options.Cookie.Name = ".lms.session";
+                options.IdleTimeout = TimeSpan.FromSeconds(600);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            services.AddDbContext<LMSContext>(options =>
+                    options.UseSqlServer("Data Source=titan.cs.weber.edu,10433;Initial Catalog=LMS_BLUE;User ID=LMS_BLUE;Password=Blue2021!"));
         }
     }
 }
+
 
